86d13a02da4c6cb58ab2488119a4ea4f8496affc
ttsutchi <tailegog@gmail.com>
2017-10-28 16:55:27 +0900

    Reload .gitignore and add discount function in VC

--- Discount/ViewController.swift
+++ Discount/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var priceField: UITextField!
+    @IBOutlet var ratioField: UITextField!
+    @IBOutlet var zeiSegment: UISegmentedControl!
+    @IBOutlet var ratioSegment: UISegmentedControl!
+    @IBOutlet var ratioLabel: UILabel!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func discount() {
+        var price: Float = Float(priceField.text!)!
+        let ratio: Float = Float(ratioField.text!)!
+        
+        var zei: Float!
+        var ratioType: Float!
+        
+        switch zeiSegment.selectedSegmentIndex {
+        case 0:
+            zei = 1.0
+        case 1:
+            zei = 1.08
+        default:
+            zei = 1.0
+        }
+        
+        price = price * zei
+        
+        switch ratioSegment.selectedSegmentIndex {
+        case 0:
+            ratioType = 100
+        case 1:
+            ratioType = 10
+        default:
+            ratioType = 100
+        }
+        
+        
+        let discount: Float = 1 - (ratio / ratioType)
+    
+        let result: Float = price * discount
+        
+        resultLabel.text = String(Int(result))
+    }
+    
+    @IBAction func switchType(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            ratioLabel.text = "%引"
+        case 1:
+            ratioLabel.text = "割引"
+        default:
+            ratioLabel.text = "%引"
+        }
+    }
+    
 }
 
